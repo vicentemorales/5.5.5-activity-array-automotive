701498315604944ee9951058b7b2d9c5690fc9d8
vicentemorales <vmorales121@gmail.com>
2023-03-09 18:19:39 -0800

    added comments

--- mecurySedan.js
+++ mecurySedan.js
@@ -2,6 +2,7 @@
 //this includes the vehicle class as a module
 const VehicleModule = require('./vehicle.js').Vehicle
 
+// create a class and add parameters
 class Car extends VehicleModule{
     constructor(make, model, year, color, mileage){
         super(make, model, year, color, mileage)
@@ -14,10 +15,13 @@ class Car extends VehicleModule{
 
     }
 
+    //methos for the class
+    //adding passengers
     loadPassenger(num){
         this.passenger = num;
     }
 
+    //start function taken from vehicle.js
     start() {
         if (this.fuel > 0) {
             console.log("engine started...!!!")
@@ -30,6 +34,7 @@ class Car extends VehicleModule{
         }
     }
 
+    //service function (mileage over 30000)
     scheduledService(){
         if (this.mileage>30000){
             this.scheduleService = true
@@ -39,14 +44,16 @@ class Car extends VehicleModule{
 
 }
 
-let myCar = new Car('mercury', 'rad_sedan', '2002', 'white', 50000)
+// create a variable who's value is a custom car class
+let honda = new Car('mercury', 'rad_sedan', '2002', 'sonic grey', 50000)
 
-myCar.start()
-myCar.loadPassenger(5)
-myCar.stop()
-myCar.scheduledService()
+//call the functions in the class
+honda.start()
+honda.loadPassenger(5) //added 5
+honda.stop()
+honda.scheduledService()
 
-console.log(myCar)
+console.log(honda) //print honda in console. (check for the change in scheduleService) 
 
 /*
 //this shows how to call from this module...
